86520eb5b59ad2289ef4ce129e792871fddd418d
Joseph Spens <joseph@spens.us>
2013-09-13 01:56:22 -0400

    Fixed Github feed URI

--- app/scripts/feed.js
+++ app/scripts/feed.js
@@ -3,7 +3,7 @@ define(['jquery', 'handlebars', 'momentjs'], function ($) {
 	'use strict';
 
 	var Feed = function () {
-		this.fetch('#github','https://github.com/josephspens.json');
+		this.fetch('#github','https://github.com/josephspens.json?callback=?');
 		this.fetch('#blog','https://www.googleapis.com/plus/v1/people/105984509136518387439/activities/public?maxResults=5&key=AIzaSyCjsAgokjM_8s_M2HMuCt2YuKSxcE9Owb8');
 
 		return this;
@@ -11,6 +11,14 @@ define(['jquery', 'handlebars', 'momentjs'], function ($) {
 
 	Feed.prototype.fetch = function (container, url) {
 		$.getJSON(url, function(data){
+			// Github JSON feed is malformed
+			if(data.hasOwnProperty('length')){
+				data = {
+					items: $.map(data, function(value, key) {
+						return value;
+					})
+				};
+			}
 			console.log(data);
 
 			$(container + ' .items').html(Handlebars.compile($(container + '-template').html())(data));
