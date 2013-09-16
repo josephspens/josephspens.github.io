d759f5b4096bea6a8cb2e646fd6b4262d21a6317
Joseph Spens <joseph@spens.us>
2013-09-16 15:34:28 -0400

    Blog article meta now stay to the right of the article image

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -77,8 +77,8 @@ function program2(depth0,data) {
     + "\">";
   if (stack1 = helpers.annotation) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.annotation; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
-  buffer += escapeExpression(stack1)
-    + "</a>\n			";
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "</a>\n			";
   return buffer;
   }
 
