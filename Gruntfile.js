403acb401839986a1a01310f473f2ffb1be7a41a
Joseph Spens <joseph@workmarket.com>
2013-12-13 21:55:10 -0500

    Removed modernizr because the build was timing out

--- Gruntfile.js
+++ Gruntfile.js
@@ -401,7 +401,7 @@ module.exports = function (grunt) {
         'concat',
         'cssmin',
         'uglify',
-        'modernizr',
+        // 'modernizr',
         'copy:dist',
         'rev',
         'usemin'
